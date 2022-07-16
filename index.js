2ac8edc9fdced778238dee6851978ee13c3d7ed3
Manya Mittal <manyamittal@Manyas-MacBook-Pro.local>
2022-07-17 04:49:08 +0530

    change files name

--- index.js
+++ index.js
@@ -1,85 +1,101 @@
 const puppeteer = require('puppeteer');
-const { scrollPageToBottom } = require('puppeteer-autoscroll-down')
+const prompt = require('prompt-sync')({ sigint: true });
 const xlsx = require('xlsx');
 
 async function scrape() {
+  
+
+  console.log('This program requires a URL from Zomato. For exmaple: https://www.zomato.com/jakarta/thamrin-restaurants');
+  var url = prompt('Please enter a Zomato URL: ');
+
   const browser = await puppeteer.launch({headless: false})
   const page = await browser.newPage()
-  url = 'https://www.zomato.com/jakarta/petogogan-kebayoran-baru-restaurants';
-
   await page.goto(url, {waitUntil: "load"}); 
 
   let restaurants = [['Name', 'Phone Number', 'Location']]; // each sub-array represents a row in excel
 
-  // await scrollPageToBottom(page, {
-  //   size: 30,
-  //   delay: 400
-  // })
+  var cards = []
+  var links =[]
 
-  // console.log(await page.evaluate('document.body.scrollHeight'));
+  var count = 8
+  if(await page.$('div > div:nth-child('+ count +') > div > div:nth-child(1) > div > div > a') == null){
+    count = 9
+  }
 
-  await page.keyboard.press('ArrowDown')
+  
+  let lastHeight = await page.evaluate('document.body.scrollHeight');
 
-  await scrollToBottom(page)
-  var cards = await page.$$('div.jumbo-tracker')
+  while (true){
+    for (let i = 0; i < 11; i++) {
+      await page.keyboard.press('ArrowDown') // scrolls down by pressing down arrow key
+    }
+    await page.waitForTimeout(2000)
+
+    try {
+      cards = await page.$$('div.jumbo-tracker')
+    } catch (error) {
+      console.log(error);
+    }
+
+    let newHeight = await page.evaluate('document.body.scrollHeight');
+    if (newHeight === lastHeight) { // height didn't increase which means that nothing new was loaded
+        break;
+    }
+    lastHeight = newHeight;
+
+  }
+
+  
   console.log(cards.length);
 
-  var links = []
-  var count = 9
-  var stoppingPoint = (cards.length/3) +  6
-  for (let i = 0; i < stoppingPoint; i++) {
-    for (let j = 0; j < 3; j++) {
-      try {
-        var selector = 'div > div:nth-child('+ count +') > div > div:nth-child('+(j+1)+') > div > div > a'
-        // await page.waitForSelector(selector)
-        links.push(await page.$eval(selector, (el) => el.href));
-      } catch (error) {
-        console.log(error);
+  var stoppingPoint = (cards.length/3) +  count
+  var startingPoint = count
+  console.log(stoppingPoint);
+  try {
+    for (let i = startingPoint; i < stoppingPoint; i++) {
+      for (let j = 0; j < 3; j++) {
+        try {
+          var selector = 'div > div:nth-child('+ count +') > div > div:nth-child('+(j+1)+') > div > div > a'
+          links.push(await page.$eval(selector, (el) => el.href));
+        } catch (error) {
+           console.log(error);
+        }
+        
       }
-      
+      count++;
     }
-    count++;
+    
+  } catch (error) {
+   console.log('links so far:' + links); 
+   console.log('links fo far length:' + links.length);
   }
+  
 
   console.log(links);
   console.log(links.length);
 
-  // for (let i = 0; i < cards.length; i++) {
-  //   await page.goto(links[i])
 
-  //   const name = await page.$eval('h1', (el) => el.textContent)
-  //   const nameSub = name.substring(0, name.length - 1)
+  for (let i = 0; i < links.length; i++) { //goes to each link and gets the data
+    await page.goto(links[i], {waitUntil: "load"})
 
-  //   const phoneNo = await page.$eval('div > section > section > article > p', (el) => el.textContent)
+    const name = await page.$eval('h1', (el) => el.textContent)
+    const nameSub = name.substring(0, name.length - 1)
 
-  //   const location = await page.$eval('div > section > section > article > section > p', (el) => el.textContent)
+    const phoneNo = await page.$eval('div > section > section > article > p', (el) => el.textContent)
+
+    const location = await page.$eval('div > section > section > article > section > p', (el) => el.textContent)
     
-  //   restaurants.push([nameSub, phoneNo, location])
-  // }
-
-  //   excelName = 'link_5.xlsx'
-  //   var workbook = xlsx.utils.book_new(); // code to save data to an excel sheet
-  //   var worksheet = xlsx.utils.aoa_to_sheet(restaurants)
-  //   xlsx.utils.book_append_sheet(workbook, worksheet)
-  //   xlsx.writeFile(workbook, excelName)
+    restaurants.push([nameSub, phoneNo, location])
+  }
   
+  excelName = 'test.xlsx' // saves into an excel file
+  var workbook = xlsx.utils.book_new(); // code to save data to an excel sheet
+  var worksheet = xlsx.utils.aoa_to_sheet(restaurants)
+  xlsx.utils.book_append_sheet(workbook, worksheet)
+  xlsx.writeFile(workbook, excelName)
 
-  await browser.close()
+
+  await browser.close() // closes the browser window
 }
 scrape()
 
-
-
-async function scrollToBottom(page) {
-  let lastHeight = await page.evaluate('document.body.scrollHeight');
-
-  while (true) {
-    await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
-    await page.waitForTimeout(2000); // sleep a bit
-    let newHeight = await page.evaluate('document.body.scrollHeight');
-    if (newHeight === lastHeight) {
-        break;
-    }
-    lastHeight = newHeight;
-}
-}
